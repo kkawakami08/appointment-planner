9a58c9b6ebb79fed9407bf5d1b3494446af7aabe
Kaori Kawakami <k.kawakami08@gmail.com>
2022-09-26 17:57:04 -0700

    color palette and font for project

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav>
+      <h1>Appointment Planner</h1>
+      <ul>
+        <li>View/Add Contacts</li>
+        <li>View/Add Appointments</li>
+      </ul>
+    </nav>
+  )
+}
