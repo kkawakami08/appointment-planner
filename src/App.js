9151170fef240da9e44f2983a5f58e798c057da2
Kaori Kawakami <k.kawakami08@gmail.com>
2022-09-26 20:01:41 -0700

    navbar outline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import NavBar from './NavBar/NavBar';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import ContactPage from './Pages/ContactPage';
+import AppointmentPage from './Pages/AppointmentPage';
 
 function App() {
   return (
-    <NavBar />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/contact" element={<ContactPage/>} />
+        <Route path="/appointment" element={<AppointmentPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
   return (
     <nav>
       <h1>Appointment Planner</h1>
       <ul>
-        <li>View/Add Contacts</li>
-        <li>View/Add Appointments</li>
+        <NavLink to="/contact">
+         <li>View/Add Contacts</li>
+        </NavLink>
+        <NavLink to="/contact" activeClassName="active">
+          <li>View/Add Appointments</li>
+        </NavLink>
       </ul>
     </nav>
   )
